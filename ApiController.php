aee90df196b2d68aaecb5f940b74fd2c263caf95
zhouxiaolong <936140285@qq.com>
2022-01-24 20:26:37 +0800

    提交12

--- ApiController.php
+++ ApiController.php
@@ -43,5 +43,6 @@ class ApiController extends BaseController
         //一个月内任务数
         $thirtyArr = $this->get_recent_date(30);
         $thismonthStart = strtotime(date('Y-m-d 00:00:00', strtotime($thirtyArr[1]))) * 1000;
+        $thismonthEnd = strtotime(date('Y-m-d 23:59:59', strtotime($thirtyArr[30]))) * 1000;
     }
 }
